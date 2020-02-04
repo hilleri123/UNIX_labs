bef13b3db7e01b4f34465f730a942842f61622d2
shurik <shurik@skotch.nirvana>
2020-02-04 17:38:47 +0300

    3 comments

--- DyadyaVova/3lab/client.cpp
+++ DyadyaVova/3lab/client.cpp
@@ -16,26 +16,32 @@
 int
 main(int argc, char* argv[])
 {
-	if (argc < 3)
+	//Мы хотим 2 плоинома + 1 это имя проги и того 3
+	if (argc != 3)
 		return 1;
 
+	//Дефолт структоура для сокетов
 	struct sockaddr_in serv_addr; 
 
 	//sd - socket descriptor
 	int sd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sd < 0) {
+		//perror пишет (ее аргемент) + (ошибку)
 		perror("socket ");
 		return 1;
 	}
+	//Заполняем ее и ставим порт 5000 потому что он должен быть больше 3к (вроде)
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(5000);
 
 	
+	//связываемся с адресом тут это локаль
 	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
 		perror("inet_pton ");
 		return 1;
 	}
 
+	//Пытаемся законектисться
 	if(connect(sd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
 		perror("connect ");
 		return 1;
@@ -50,11 +56,13 @@ main(int argc, char* argv[])
 
 	//std::cout << argv[1] << "(" << strlen(argv[1]) << ") " << argv[2] << "(" << strlen(argv[2]) << ")" << std::endl;
 
+	//Пишем туда первый полином
 	if (write(sd, first.c_str(), strlen(first.c_str())) < 0) {
 		perror("write");
 		return 1;
 	}
 
+	//Читаем что первый полином приняли а то если мы отправим сразу второй они могут слиться в один
 	if ((n = read(sd, buf, sizeof(buf)-1)) >= 0) {
 		//buf[n] = 0;
 		//std::cout << buf << std::endl;
@@ -64,11 +72,13 @@ main(int argc, char* argv[])
 	}
 
 
+	//Пишем туда второй полином
 	if (write(sd, second.c_str(), strlen(second.c_str())) < 0) {
 		perror("write");
 		return 1;
 	}
 
+	//Читаем и выводим ответ
 	if ((n = read(sd, buf, sizeof(buf)-1)) >= 0) {
 		buf[n] = 0;
 		std::cout << buf << std::endl;
--- DyadyaVova/3lab/server.cpp
+++ DyadyaVova/3lab/server.cpp
@@ -22,44 +22,54 @@ int main(int argc, char *argv[])
 
 	const int N = 256;
 	char buf[N+1];
-	time_t ticks; 
 
+	//Делаем сокет
 	ld = socket(AF_INET, SOCK_STREAM, 0);
 
+	//Заполняем структуру на локаль (если я правильно понял)
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(5000); 
 
+	//Биндимся
 	bind(ld, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
+	//Начинаем слушать
 	listen(ld, 10); 
 
+	//Она не нужна но нам нужно что-то послать на то что мы приняли первый полином и можно слать второй 
+	//Пусть мы пошлем что-то внятное +-
 	std::string hello = "send moreee";
 
+	//На сервере не нужно чекать ошибки на все равно что ты нам сделаешь мы на сервере
 	while(1)
 	{
+		//Принимаем соединение
 		cd = accept(ld, (struct sockaddr*)nullptr, NULL); 
 
-		//ticks = time(NULL);
-		//snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
-		//write(cd, buf, strlen(buf)); 
+		//Читаем первый полином
 		read(cd, buf, N);
-		//std::cout << buf << std::endl;
+		//Собираем его из строки (класс Polynom сделает все сам)
 		Polynom a = Polynom(std::string(buf));
+		//Очистим буфер чтоб если следующее сообщение меньше этого мы не оставили свой мусор
 		memset(&buf, 0, N+1);
 
+		//Пишем что прочитали первый полином
 		write(cd, hello.c_str(), strlen(hello.c_str()));
 
+		//Читаем второй
 		read(cd, buf, N);
-		//std::cout << buf << std::endl;
 		Polynom b = Polynom(std::string(buf));
 		memset(&buf, 0, N+1);
 
+		//Умножаем 
 		Polynom c = a * b;
-		//c.print();
+		//Пишем ответ
 		write(cd, c.string().c_str(), strlen(c.string().c_str()));
 
+		//Закрываем соединение 
 		close(cd);
+		//спис 1 сек
 		sleep(1);
 	}
 }
